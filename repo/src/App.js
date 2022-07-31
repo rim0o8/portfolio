97adefee08d6c1a557390ef3c14dc9c7b16576b5
Yuuri Kurashima <yuuri.kurashima@elyza.ai>
2022-08-01 00:38:51 +0900

    clean

--- repo/src/App.js
+++ repo/src/App.js
@@ -12,7 +12,6 @@ function App() {
         <h2>Yuuri Kurashima</h2>
         <img className="large-logo" src="./yuuri.png"></img>
         Keywords: NLP, ML
-        <h3>Summary</h3>
         <h3>My Apps</h3>
         <div>
           <MyAppBtn
@@ -45,11 +44,10 @@ function App() {
               logo_src="./elyza.png"
               title="ELYZA.inc"
               link="https://elyza.ai/"
-              position="NLP AI ENGINEER"
+              position="NLP AI ENGINEER Intern"
               description="NLP, ML, RetailTech, Crawling"
           />
         </div>
-        <h3>History</h3>
         <h3>Accounts</h3>
         <div>
           <SNSLink
